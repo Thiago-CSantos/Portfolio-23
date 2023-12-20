cda51747217c163a25bf0c8f8a3b01a4dd13123a
Thiago-CSantos <thiagocarretero433@gmail.com>
2023-12-20 17:07:56 -0300

    build: portfólio pronto/quase

---
+++ src/typescript/index.js
@@ -0,0 +1,12 @@
+function redirectToRepo() {
+    window.open("https://github.com/Thiago-CSantos/ApiBolsaValores", "_blank");
+}
+var link = document.getElementById("linkToRepo");
+if (link != null) {
+    link.addEventListener("click", function () {
+        redirectToRepo();
+    });
+}
+else {
+    console.error("Elemento com ID 'linkToRepo' não encontrado no DOM.");
+}
